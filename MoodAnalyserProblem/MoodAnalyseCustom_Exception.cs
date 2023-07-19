b6d59ebfc9f2b80d657e7ca5da1e69e81f81c195
sipra <sipramishra085@gmail.com>
2023-07-19 08:56:05 +0530

    [Sipra]Add:Should throw moodanalysisException

--- MoodAnalyserProblem/MoodAnalyseCustom_Exception.cs
+++ MoodAnalyserProblem/MoodAnalyseCustom_Exception.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyserProblem
 {
-    internal class MoodAnalyseCustom_Exception
-    {
-    }
+   
+        //Custom exception are user defined exceptions use to customize the exception as
+        //per the user needed.
+        public class MoodAnalyseCustom_Exception : Exception
+        {
+            public enum MoodAnalyser_ExceptionType
+            {
+                NULL_MOOD,
+                EMPTY_MOOD,
+            }
+            public MoodAnalyser_ExceptionType type;
+            //Base - This keyword  is used to access member of a base class  with in a derived class
+            public MoodAnalyseCustom_Exception(string message, MoodAnalyser_ExceptionType type) : base(message)
+            {
+
+                this.type = type;
+
+            }
+
+        }
+    
 }
--- MoodAnalyserProblem/MoodAnalyserDemo.cs
+++ MoodAnalyserProblem/MoodAnalyserDemo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static MoodAnalyserProblem.MoodAnalyseCustom_Exception;
 
 namespace MoodAnalyserProblem
 {
@@ -28,7 +29,8 @@ namespace MoodAnalyserProblem
             }
             catch (NullReferenceException)
             {
-                return "Happy";
+                // return "Happy";
+                throw new MoodAnalyseCustom_Exception("Message should not be null", MoodAnalyser_ExceptionType.NULL_MOOD);
             }
             
         }
